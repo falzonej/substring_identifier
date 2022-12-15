d711d3bf13e54c69960d21e667c58f852e654ce0
JoeFalzone <falzonej@gmail.com>
2022-12-15 17:09:21 -0500

    cleaned and renamed variables and funcs

--- substring_identifier.rb
+++ substring_identifier.rb
@@ -10,7 +10,7 @@ def split_sentence_string(sentence)
   end
 end
 
-def split_dic_arr(dictionary)
+def split_dic_array(dictionary)
   dictionary.map do |string|
     string.split(//)
   end
@@ -18,7 +18,6 @@ end
 
 def check_word(string, substring)
 found_letters = []
-found_word = []
   substring.each do |substring_value|
     string.each do |string_value|
       if substring_value == string_value
@@ -30,17 +29,6 @@ found_word = []
   found_letters.join()
 end
 
-def check_found_words_against_substring(unchecked_words, dictionary)
-  found_words = []
-  dictionary.each do |unchecked_word|
-    unchecked_words.each do |dictionary_word|
-      if unchecked_word = dictionary_word
-        found_words << unchecked_word
-      end
-    end
-  end
-end
-
 def string_checking(split_sentence, split_dictionary)
   found_strings = []
   split_dictionary.each do |dic_substring|
@@ -48,22 +36,48 @@ def string_checking(split_sentence, split_dictionary)
       found_strings << check_word(sentence_string, dic_substring)
     end
   end
-  complete_substrings = found_strings.reject{ |string| string.empty? }.uniq
+  found_strings.reject{ |string| string.empty? }
+end
+
+def check_found_words_against_substring(unchecked_words, dictionary)
+  found_words = []
+  unchecked_words.each do |unchecked_string|
+    dictionary.each do |dictionary_string|
+      if dictionary_string == unchecked_string
+        found_words << unchecked_string
+      end
+    end
+  end
+  found_words
+end
+
+def add_found_words_to_hash(all_found_words)
+  final_hash = 0
+  all_found_words.reduce(Hash.new(0)) do |result, word|
+    result[word] += 1
+    final_hash = result
+  end
+  final_hash
 end
 
 
 split_sentence = split_sentence_string(sentence)
-split_dictionary = split_dic_arr(dictionary)
+split_dictionary = split_dic_array(dictionary)
 unchecked_words = string_checking(split_sentence, split_dictionary)
+all_found_words = check_found_words_against_substring(unchecked_words, dictionary)
 
 
+
+#=== Tests Below Here ===
+
+p add_found_words_to_hash(all_found_words)
 #substring = 'app'.split(//)
 #string = 'apples'.split(//)
-
-p check_found_words_against_substring(unchecked_words, dictionary)
+#check_found_words_against_substring(unchecked_words, dictionary)
 #p string_checking(split_sentence, split_dictionary)
 #p check_word(string, substring)
 #p split_dictionary
 #p sentence_splitting(sentence)
 #p dictionary_splitting(dictionary)
+#p unchecked_words
 

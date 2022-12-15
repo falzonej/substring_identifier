0667ee3f60bde89d82554e35129bc49bf25a5e4f
JoeFalzone <falzonej@gmail.com>
2022-12-15 16:07:44 -0500

    add check_found_words_against_substring()

--- substring_identifier.rb
+++ substring_identifier.rb
@@ -1,27 +1,27 @@
 require 'pry'
 
-sentence = 'go good apples'
-dictionary = ['go', 'good', 'app', 'apple', 'apples']
+sentence = "Howdy partner, sit down! How's it going?"
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def sentence_splitting(sentence)
+def split_sentence_string(sentence)
   split_sentence = sentence.split(/ /)
   split_sentence.map do |word|
     word.split(//)
   end
 end
 
-def dictionary_splitting(dictionary)
+def split_dic_arr(dictionary)
   dictionary.map do |string|
     string.split(//)
   end
 end
 
-def word_checking(string, substring)
+def check_word(string, substring)
 found_letters = []
+found_word = []
   substring.each do |substring_value|
     string.each do |string_value|
       if substring_value == string_value
-        
         found_letters << substring_value
         break
       end
@@ -30,28 +30,39 @@ found_letters = []
   found_letters.join()
 end
 
-def string_checking(split_sentence, split_dictionary)
-  found_strings = []
-  split_dictionary.map do |string|
-      if split_sentence.any?(string)
-          found_strings << string.join()
+def check_found_words_against_substring(unchecked_words, dictionary)
+  found_words = []
+  dictionary.each do |unchecked_word|
+    unchecked_words.each do |dictionary_word|
+      if unchecked_word = dictionary_word
+        found_words << unchecked_word
       end
-    p found_strings
+    end
   end
 end
 
+def string_checking(split_sentence, split_dictionary)
+  found_strings = []
+  split_dictionary.each do |dic_substring|
+    split_sentence.each do |sentence_string|
+      found_strings << check_word(sentence_string, dic_substring)
+    end
+  end
+  complete_substrings = found_strings.reject{ |string| string.empty? }.uniq
+end
 
 
-split_sentence = sentence_splitting(sentence)
-split_dictionary = dictionary_splitting(dictionary)
-
-substring = 'apple'.split(//)
-string = 'apples'.split(//)
+split_sentence = split_sentence_string(sentence)
+split_dictionary = split_dic_arr(dictionary)
+unchecked_words = string_checking(split_sentence, split_dictionary)
 
 
+#substring = 'app'.split(//)
+#string = 'apples'.split(//)
 
-#string_checking(split_sentence, split_dictionary)
-#p word_checking(string, substring)
+p check_found_words_against_substring(unchecked_words, dictionary)
+#p string_checking(split_sentence, split_dictionary)
+#p check_word(string, substring)
 #p split_dictionary
 #p sentence_splitting(sentence)
 #p dictionary_splitting(dictionary)
